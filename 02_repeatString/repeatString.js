54e55835c4ae912bc190f717cc56dd7fcee9e9b5
DJV <djtvaq@gmail.com>
2022-07-22 20:13:44 -0400

    Create while loop to multiply the repeated string. Add if conditional to filter negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,29 @@
 // take the string 
 // concatenate the string in a loop, using num as the end condition
 
-const repeatString = function(string, num) {
+const repeatString = function (string, num) {
+//TWO WAYS TO DO IT
+    if (num < 0) {
+        return 'ERROR'
+    } else {
+        return string.repeat(num)
+    }
+}
 
-    return string.repeat(num)
 
 
-};
+//     let emptyString = '';
+//     if (num < 0) {
+//         return 'ERROR'
+//     } else {
+//         while (num > 0) {
+//             emptyString += string
+//             num--
+//         }
+//     }
+//     return emptyString;
+
+// };
 
 // Do not edit below this line
 module.exports = repeatString;
