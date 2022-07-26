27a80de998b55b965df78c9a7e328a9cab5a6213
DJV <djtvaq@gmail.com>
2022-07-25 21:35:03 -0400

    Create for loop to search for and splice filtered value from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function([array], filter) {
+
+    for (let i=0; i < array.length; i++) {
+        if (arguments[i] === filter) {
+            array.splice(i, 1)
+            return array
+        } else {
+            return array
+        }
+    }
 
 };
 
