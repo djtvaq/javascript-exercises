f0beaafde09eb4896e8b0edba27e121a3e40a92c
DJV <djtvaq@gmail.com>
2022-07-25 21:23:00 -0400

    Add blank '' seperator to reverseCharacters.join()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,13 @@
-const reverseString = function(string) {
+const reverseString = function (string) {
     let characters = string.split('')
     let reverseCharacters = characters.reverse()
-    let stringReversed = reverseCharacters.join()
+    let stringReversed = reverseCharacters.join('')
 
     return stringReversed
 
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
