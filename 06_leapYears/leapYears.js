1278f0ac1ea009ca803a3875357392b98064108c
DJV <djtvaq@gmail.com>
2022-07-26 22:56:28 -0400

    Create function that determines if a year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
-};
+        if (year % 4 === 0 && year % 400 === 0) {
+            //400 800 1200 etc
+            return true
+        } else if (year % 4 === 0 && year % 100 !==0) {
+            return true
+        } else {
+            return false
+        }
+ };
 
+//1900 and 700 come back as true
 // Do not edit below this line
 module.exports = leapYears;
+
+//if a year is div
