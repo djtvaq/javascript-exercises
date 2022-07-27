251f6cbd1ca65ee3d08a62fa1e2a386210c9e76f
DJV <djtvaq@gmail.com>
2022-07-26 21:45:51 -0400

    Use array.filter to find difference between both arrays

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,23 @@
-const removeFromArray = function(array, filter) {
+//Take an array of data from the user
+//Accept filters element from user
+//Loop through the array of data and look for elements that match any of the filter elements
+//Remove those elements from the array
+//Return the new array
 
-console.log(array)
-console.log(filter)
 
-let i = 0;
+const removeFromArray = function (array, ...filter) {
 
-while (i < array.length) {
+    let filterArray = Array.from(filter)
 
-    if (array[i] === filter) {
-        let splicedArray = array.splice(i,1)
-        return array
-    }
-    i++
+//x = elements from array, if filterArray(element) does not have x, return true, it passes the filter test
+//return difference = return all the elements that passed our filter test
+    let difference = array.filter(x => !filterArray.includes(x));
+
+return difference
+ 
 }
-}
+
+
 
 
 // Do not edit below this line
