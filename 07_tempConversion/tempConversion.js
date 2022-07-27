9c8f62c927acb78235d265eac4746ae98edd7ffb
DJV <djtvaq@gmail.com>
2022-07-26 23:18:49 -0400

    Create a function that converts f to c and c to f to one decimal point

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+let farToCelPrecice = ((f-32)*(5/9))
+let celString = farToCelPrecice.toFixed(1)
+return parseFloat(celString)
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+let celToFarPrecice = (c*(9/5)+32)
+let farString = celToFarPrecice.toFixed(1)
+return parseFloat(farString)
 };
 
 // Do not edit below this line
