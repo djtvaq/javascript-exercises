8de6756854a8680af547c0067d04d5b17cfc2790
DJV <djtvaq@gmail.com>
2022-07-26 22:15:00 -0400

    create function to add sum of all integers between two inputs

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
 
+    let largestNumber = Math.max(a, b)
+    let smallestNumber = Math.min(a, b)
+
+    let range = [];
+    for (let i = smallestNumber; i <= largestNumber; i++) {
+        range.push(i)
+    }
+
+    let initialValue = 0
+    let totalSum = range.reduce((previousValue, nextValue) =>
+        previousValue + nextValue, initialValue
+    )
+
+    if (a < 0 || b < 0) {
+        return 'ERROR'
+    } else if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR'
+    } else {
+        return totalSum
+    }
+  
 };
 
 // Do not edit below this line
