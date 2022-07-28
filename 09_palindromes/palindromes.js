b564824ffbf300ad853f345601d49c34d61d9e08
DJV <djtvaq@gmail.com>
2022-07-27 22:37:40 -0400

    Fixed bug that caused the reverse array to overwrite original array, causing test to always post true

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,17 +1,41 @@
+
+
 const palindromes = function (string) {
-//take the inpur variable and split characters in array
-//flip the array 
-//compare if the flipped array matches the original array
 
-let cleanString = string.replace(/[!,@,#,$,%,^,&,*,(,),?,.,',"," ", ,]/g,'')
-let cleanArray = cleanString.split('')
-let reverseArray= cleanArray.reverse()
+    //take the inpur variable and split characters in array
+    //flip the array 
+    //compare if the flipped array matches the original array
 
-if (cleanArray == reverseArray) {
-    return true
-} else {
-    return false
-}
+    
+    //remove all punctuation
+    let cleanString = string.replace(/[!,@,#,$,%,^,&,*,(,),?,.,',"," ", ,]/g, '')
+    console.log(cleanString)
+
+    //turn string into array of characters
+    let characterArray = cleanString.split('')
+    console.log(characterArray)
+
+    //make every character in array lowercase
+    let cleanArray = characterArray.map(x => x.toLowerCase())
+    console.log(cleanArray)
+
+    //create new array to reverse 
+    let copyCleanArray = characterArray.map(x => x.toLowerCase())
+    console.log(copyCleanArray)
+
+    //reverse the new array
+    let reverseArray = copyCleanArray.reverse()
+
+    let testVar1 = cleanArray.join('')
+    let testVar2 = reverseArray.join('')
+
+
+    if (testVar1 === testVar2) {
+        return true
+    } else {
+        return false
+    }
+
 };
 
 // Do not edit below this line
