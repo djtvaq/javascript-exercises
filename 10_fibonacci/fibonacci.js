d924f864f01d5933dd9a68fe4a2b67460762c1a0
DJV <djtvaq@gmail.com>
2022-07-28 00:05:40 -0400

    create function which returns fibonacci number at any point in sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (a) {
+
+
+    if (a < 0) {
+        return 'OOPS'
+    }
+
+    //first two of the fibonacci are fixed according to formula
+    let fibonacciStarter = [1, 1]
+
+    //formula = fn = fn-1 + fn-2. this formulat iterates this formulat for ever number starting at 2 ( which is where this formula starts, at f3 in the sequence)
+    for (let i = 2; i < a; i++) {
+        let fibonacciFormula = fibonacciStarter[i - 1] + fibonacciStarter[i - 2];
+        fibonacciStarter.push(fibonacciFormula)
+    }
+
+    //returm the last number of the sequence
+    return fibonacciStarter[fibonacciStarter.length - 1]
+
+
+
 
 };
 
