bd54c7f7da93366ed7d7cdaca5a9dec337c5b8ac
DJV <djtvaq@gmail.com>
2022-08-02 23:07:39 -0400

    Allow large number arguements, wrap index to 0 when end of array is reached

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,33 +1,43 @@
-const caesar = function(string, number) {
+const caesar = function (string, number) {
 
 
-    let alphabetLowerCase =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+    let alphabetLowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
     let alphabetUpperCase = alphabetLowerCase.map(x => x.toUpperCase())
 
     let stringCharacterArray = string.split('')
-  
-    let encryptedArray =[]
 
-    for (let i=0; i <stringCharacterArray.length; i++) {
+    let encryptedArray = []
 
-        if (stringCharacterArray[i] == stringCharacterArray[i].toLowerCase()){
-            let characterIndex = alphabetLowerCase.indexOf(stringCharacterArray[i])
-            let encryptedCharacter = alphabetLowerCase[characterIndex+number]
 
-            encryptedArray.push(encryptedCharacter)
+    for (let i = 0; i < stringCharacterArray.length; i++) {
 
-        } else if (stringCharacterArray[i] !== stringCharacterArray[i].toLowerCase()) {
-            let characterIndex = alphabetUpperCase.indexOf(stringCharacterArray[i])
-            let encryptedCharacter = alphabetUpperCase[characterIndex+number]
 
-            encryptedArray.push(encryptedCharacter)
+        if (alphabetLowerCase.includes(stringCharacterArray[i].toLowerCase())) {
+
+            if (stringCharacterArray[i] == stringCharacterArray[i].toLowerCase()) {
+                let characterIndex = alphabetLowerCase.indexOf(stringCharacterArray[i])
+                let loopedCharacterIndex = (characterIndex+number)%alphabetLowerCase.length
+
+
+                let encryptedCharacter = alphabetLowerCase[loopedCharacterIndex]
+
+                encryptedArray.push(encryptedCharacter)
+
+            } else if (stringCharacterArray[i] !== stringCharacterArray[i].toLowerCase()) {
+                let characterIndex = alphabetUpperCase.indexOf(stringCharacterArray[i])
+                let loopedCharacterIndex = (characterIndex+number)%alphabetUpperCase.length
+                let encryptedCharacter = alphabetUpperCase[loopedCharacterIndex]
+
+                encryptedArray.push(encryptedCharacter)
+            }
+
         } else {
             encryptedArray.push(stringCharacterArray[i])
         }
 
     }
-
+    console.log(encryptedArray)
 
     return `${encryptedArray.join('')}`
     //take a string
@@ -39,3 +49,41 @@ const caesar = function(string, number) {
 
 // Do not edit below this line
 module.exports = caesar;
+
+
+//count = (count + randomNumber) % colours.length
+
+// new inxed = (current index + number) % length
+
+
+//lets say we have z in position 25, and we want to add 2
+//25+2= 27
+//27 %26 = 1
+
+// lets say we have a in position 0, and wany to add -2
+//25 + -2 = 23
+// 23 % 26 = 3
+
+//to make negatives work
+//currentindex % alphabetindex) +alphabetuindex) %alpbahetindex)
+//26 % -2 + -2
+
+
+// we are in postion 0 (a) and watn to go back 2 (y)
+// so the number we are looking for is 24
+//currentIndex(00)+(-2%26)
+//currentIndex(oo)+(26+(-2%26))
+
+// we are in postion 0 (a) and watn to go back 28(x or y)
+// so the number we are looking for is 24
+//currentIndex(00)+(-28%26)
+//currentIndex(oo)+(26+(-28%26))
+
+//currentIndex =(alphabet.length+(number%alphabet.length))
+
+//foo[(i % foo.count + foo.count) % foo.count])
+
+//alphabetLowerCase[(number % alphabetLowerCase[i]) % alphabetLowerCase[i]]
+
+//exambple, we have lwoercase b [1] and want to -2, to z[25]
+//encryptedCharacter = (-2 % 1 + 1) % 1
