4771e6dc74cef355fa5bf347cc5001375b62ce37
DJV <djtvaq@gmail.com>
2022-08-02 20:40:07 -0400

    Create a function that determins who is the oldest of people (objects) in an array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+
+//Make a new array of everyones ages
+// find the index of the largest age
+// return the index of the original array to find whos the oldest. 
+
+let ageArray = []
+
+for (let i=0; i < people.length; i++) {
+
+    ageArray.push(people[i].yearOfDeath - people[i].yearOfBirth)
+
+}
+
+console.log(ageArray)
+let oldestAge = Math.max(...ageArray)
+
+console.log(oldestAge)
+let index = ageArray.indexOf(oldestAge)
+
+      console.log(index)
+
+let oldestPerson = people[index]
+ 
+return oldestPerson
 };
 
 // Do not edit below this line
