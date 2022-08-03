54221d9e7bb1fe4966087585dfac31eb6487bfe5
DJV <djtvaq@gmail.com>
2022-08-02 23:10:19 -0400

    Adjust formatting

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -17,7 +17,7 @@ const caesar = function (string, number) {
 
             if (stringCharacterArray[i] == stringCharacterArray[i].toLowerCase()) {
                 let characterIndex = alphabetLowerCase.indexOf(stringCharacterArray[i])
-                let loopedCharacterIndex = (characterIndex+number)%alphabetLowerCase.length
+                let loopedCharacterIndex = (characterIndex + number) % alphabetLowerCase.length
 
 
                 let encryptedCharacter = alphabetLowerCase[loopedCharacterIndex]
@@ -26,7 +26,7 @@ const caesar = function (string, number) {
 
             } else if (stringCharacterArray[i] !== stringCharacterArray[i].toLowerCase()) {
                 let characterIndex = alphabetUpperCase.indexOf(stringCharacterArray[i])
-                let loopedCharacterIndex = (characterIndex+number)%alphabetUpperCase.length
+                let loopedCharacterIndex = (characterIndex + number) % alphabetUpperCase.length
                 let encryptedCharacter = alphabetUpperCase[loopedCharacterIndex]
 
                 encryptedArray.push(encryptedCharacter)
@@ -49,41 +49,3 @@ const caesar = function (string, number) {
 
 // Do not edit below this line
 module.exports = caesar;
-
-
-//count = (count + randomNumber) % colours.length
-
-// new inxed = (current index + number) % length
-
-
-//lets say we have z in position 25, and we want to add 2
-//25+2= 27
-//27 %26 = 1
-
-// lets say we have a in position 0, and wany to add -2
-//25 + -2 = 23
-// 23 % 26 = 3
-
-//to make negatives work
-//currentindex % alphabetindex) +alphabetuindex) %alpbahetindex)
-//26 % -2 + -2
-
-
-// we are in postion 0 (a) and watn to go back 2 (y)
-// so the number we are looking for is 24
-//currentIndex(00)+(-2%26)
-//currentIndex(oo)+(26+(-2%26))
-
-// we are in postion 0 (a) and watn to go back 28(x or y)
-// so the number we are looking for is 24
-//currentIndex(00)+(-28%26)
-//currentIndex(oo)+(26+(-28%26))
-
-//currentIndex =(alphabet.length+(number%alphabet.length))
-
-//foo[(i % foo.count + foo.count) % foo.count])
-
-//alphabetLowerCase[(number % alphabetLowerCase[i]) % alphabetLowerCase[i]]
-
-//exambple, we have lwoercase b [1] and want to -2, to z[25]
-//encryptedCharacter = (-2 % 1 + 1) % 1
